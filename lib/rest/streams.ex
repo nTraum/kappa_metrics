716537c222ad9964dd69d644eeadb8cf85e19bf4
Philipp Preß <philipp.press@googlemail.com>
2016-02-08 09:23:33 +0100

    Add tests for `KappaMetrics.Rest.Streams` module

--- lib/rest/streams.ex
+++ lib/rest/streams.ex
@@ -1,6 +1,17 @@
 defmodule KappaMetrics.Rest.Streams do
+  @moduledoc """
+  Represents the Twitch REST API v3 'streams' endpoint.
+  """
+
   use HTTPoison.Base
 
+  @doc """
+  Returns the full HTTP API URL for the given stream.
+
+  ## Examples
+      iex> KappaMetrics.Rest.Streams.process_url("rocketbeanstv")
+      "https://api.twitch.tv/kraken/streams/rocketbeanstv"
+  """
   def process_url(name) do
     "https://api.twitch.tv/kraken/streams/" <> name
   end
@@ -9,5 +20,14 @@ defmodule KappaMetrics.Rest.Streams do
     body
     |> Poison.decode!
     |> Map.get("stream")
+    |> add_online_status
+  end
+
+  defp add_online_status(data) when is_nil(data) do
+    %{"online" => false}
+  end
+
+  defp add_online_status(data) when is_map(data) do
+    Map.put(data, "online", true)
   end
 end
---
+++ test/rest/streams_test.exs
@@ -0,0 +1,6 @@
+defmodule KappaMetrics.Rest.StreamsTest do
+  use ExUnit.Case, async: true
+
+  alias KappaMetrics.Rest.Streams
+  doctest Streams
+end
